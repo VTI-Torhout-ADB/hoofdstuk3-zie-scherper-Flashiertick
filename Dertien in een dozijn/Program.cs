7f9080374138f83b3b016ea50c4db08df97a6b62
Brent Flameyn <flameynbrent@leerling.sint-rembert.be>
2025-02-10 16:56:01 +0100

    Dertien in een dozijn gemaakt

--- Dertien in een dozijn/Program.cs
+++ Dertien in een dozijn/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int doosGrootte = 8;
+            int aantalEieren = 347;
+
+            Console.WriteLine($"Je kan {aantalEieren / doosGrootte} dozen vullen.");
+            Console.WriteLine($"Je hebt {aantalEieren % doosGrootte} eieren over.");
         }
     }
 }
