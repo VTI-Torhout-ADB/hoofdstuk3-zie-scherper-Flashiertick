6cbf22284d39249979559f6ac8ca35d5a48a8c95
Brent Flameyn <flameynbrent@leerling.sint-rembert.be>
2025-02-10 16:45:37 +0100

    Mad libs gemaakt

--- Mad Libs/Program.cs
+++ Mad Libs/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Geef een naam. ");
+            string naam = Console.ReadLine();
+            Console.WriteLine("Geef een zelfstandig naamwoord. ");
+            string Zn = Console.ReadLine();
+            Console.WriteLine("Geef een adjectief. ");
+            string adjectief = Console.ReadLine();
+            Console.WriteLine("Geef een werkwoord. ");
+            string werkwoord = Console.ReadLine();
+
+            Console.WriteLine($"Op een dag ging {naam} naar de AP Hogeschool. Hij zag daar een {adjectief} {Zn} en vond dat zo grappig dat hij begon te {werkwoord}.");
         }
     }
 }
