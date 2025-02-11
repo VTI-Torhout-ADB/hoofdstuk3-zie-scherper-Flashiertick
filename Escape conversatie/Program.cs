d19fecad64a86372a84d11ed1e476ce1eb344bc6
Brent Flameyn <flameynbrent@leerling.sint-rembert.be>
2025-02-11 17:22:31 +0100

    escape conversatie klaar

--- Escape conversatie/Program.cs
+++ Escape conversatie/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string personage1 = "Alice";
+            string personage2 = "Bob";
+            string dialoog = $"{personage1}: \"Hoe gaat het met je?\"\n\t{personage2}: \"Goed, dank je! Hoe gaat het met jou?\"\n\t{personage1}:\"Ook goed, bedankt dat je het vraagt.";
+Console.WriteLine(dialoog);
         }
     }
 }
